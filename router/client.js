bb4aa5c672cafb59200a5a12ffc2c0dac876c9c7
danny <dannyivanc@gmail.com>
2023-11-13 01:59:30 -0400

    modificando front

--- router/client.js
+++ router/client.js
@@ -5,8 +5,8 @@ const md_auth = require("../middlewares/authenticated");
 const api = express.Router();
 
 api.get("/clients", [md_auth.asureAuth], ClientController.getClients);
-api.post("/client", [md_auth.asureAuth,md_auth.roleAuth], ClientController.createClient);
-api.patch("/client/:id",[md_auth.asureAuth,md_auth.roleAuth],ClientController.updateClient);
-api.delete("/client/:id", [md_auth.asureAuth,md_auth.roleAuth], ClientController.deleteClient);
+api.post("/client", [md_auth.asureAuth], ClientController.createClient);
+api.patch("/client/:id",[md_auth.asureAuth],ClientController.updateClient);
+api.delete("/client/:id", [md_auth.asureAuth], ClientController.deleteClient);
 
 module.exports = api;
--- router/user.js
+++ router/user.js
@@ -5,7 +5,7 @@ const md_auth = require("../middlewares/authenticated");
 const api = express.Router();
 
 api.get("/user/me", [md_auth.asureAuth], UserController.getMe);
-api.get("/users", [md_auth.asureAuth], UserController.getUsers);
+api.get("/users", [md_auth.asureAuth,md_auth.roleAuth], UserController.getUsers);
 api.post("/user", [md_auth.asureAuth,md_auth.roleAuth], UserController.createUser);
 api.patch("/user/:id",[md_auth.asureAuth,md_auth.roleAuth],UserController.updateUser);
 api.delete("/user/:id", [md_auth.asureAuth,md_auth.roleAuth], UserController.deleteUser);

4ae12a5355acbfb9c86ebcaf60ac24cdf4b0a906
danny <dannyivanc@gmail.com>
2023-11-30 00:33:09 -0400

    v.2

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
-
-// Import routings
 const authRoutes = require("./router/auth");
 const userRoutes = require("./router/user");
 const clientRoutes = require("./router/client")
@@ -16,7 +13,6 @@ const courseRoutes = require("./router/course");
 const postRoutes = require("./router/post");
 const newsletterRoutes = require("./router/newsletter");
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -25,6 +21,7 @@ app.use(express.static("uploads"));
 
 
 app.use(cors());
+app.use('/',authRoutes)
 app.use(`/api`, authRoutes);
 app.use(`/api`, userRoutes);
 app.use(`/api`, clientRoutes);

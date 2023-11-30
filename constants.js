6df36b7337960b92ad67279aa05f35d518852cc6
danny <dannyivanc@gmail.com>
2023-11-30 00:37:30 -0400

    v.2asd

--- constants.js
+++ constants.js
@@ -14,7 +14,8 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 // const API_VERSION = process.env.API_VERSION;
 const IP_SERVER = process.env.IP_SERVER;
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const JWT_SECRET_KEY ="gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA"
 const IP_DATABASE=process.env.IP_DATABASE;
 const PORT_DB = process.env.PORT_DB;
 
